ff73d2c0d80f7f17e66e2498510bb80cb9a48d0f
ZainZ <zulfiqar.zain@hotmail.com>
2015-12-20 23:13:41 +0000

    Updated validation of task title

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,5 +1,5 @@
 class Task < ActiveRecord::Base
   
-  belongs_to :user
+ validates :title, presence: true
   
 end
---
+++ test/models/task_test.rb
@@ -0,0 +1,14 @@
+require 'test_helper'
+
+class TaskTest < ActiveSupport::TestCase
+  
+  def setup
+    @task = Task.new(title: "Pickup string", details: "I like string. So I need to buy some from the dollar store.")
+  end
+  
+  test "Title field cannot be blank" do
+    @task.title = ""
+    assert_not @task.valid?
+  end
+  
+end
