e8793487a4c5b6f00d1acd08007037384b2981a4
ZainZ <zulfiqar.zain@hotmail.com>
2015-12-21 05:29:21 +0000

    Updated core validation components of User and Task models

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,5 +3,6 @@ class Task < ActiveRecord::Base
   belongs_to :user
   
   validates :title, presence: true
+  validates :user_id, presence: true
   
 end
--- test/models/task_test.rb
+++ test/models/task_test.rb
@@ -3,11 +3,17 @@ require 'test_helper'
 class TaskTest < ActiveSupport::TestCase
   
   def setup
-    @task = Task.new(title: "Pickup string", details: "I like string. So I need to buy some from the dollar store.")
+    @user = User.create(name: "Anyone", email: "anyone@example.com")
+    @task = @user.tasks.create(title: "Pickup string", details: "I like string. So I need to buy some from the dollar store.")
   end
   
   test "Title field cannot be blank" do
     @task.title = " "
+    assert_not @task.valid?
+  end
+  
+  test "Ensure a user is associated with any task created" do
+    @task.user_id = nil
     assert_not @task.valid?
   end
   
