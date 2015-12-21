ba8d3fac6015ce748a2ea10620b9f5a46e7c7e2d
ZainZ <zulfiqar.zain@hotmail.com>
2015-12-21 23:31:46 +0000

    Designed the pages to display all enteries in database. Controllers updated to route between pages.

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,6 +2,13 @@ class TasksController < ApplicationController
   
   def index
     @tasks = Task.all
+    @userAccount = @tasks.first.user
+  end
+  
+  def show
+    # Params is an entie HASH passed into the link. We find the ID 
+    # component in the HASH to find the according task object/entery in the db.
+    @task = Task.find(params[:id])
   end
   
 end
