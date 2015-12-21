3aebecf12f63ed4fadefd826cc766588374961c1
ZainZ <zulfiqar.zain@hotmail.com>
2015-12-21 06:42:06 +0000

    Update html files

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,7 @@
+class TasksController < ApplicationController
+  
+  def index
+    @tasks = Task.all
+  end
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,17 @@
 Rails.application.routes.draw do
   
   root 'pages#home'
-  get '/home', to: 'pages#home'
+  get '/home', to: 'pages#home'             #main home/login page
+  
+  
+  # get '/tasks', to: 'tasks#index'           #display all tasks
+  # get '/tasks/new', to: 'tasks#new'         #new form to create a new task
+  # post '/tasks', to: 'tasks#create'         #handle HTTP request to create a new task
+  # get '/tasks/:id/edit', to: 'tasks#edit'   #edit the task
+  # patch '/tasks/:id', to: 'tasks#update'    #handle updating records when user edits task
+  # get '/tasks/:id', to: 'tasks#show'        #show individial task details
+  # delete '/tasks/:id', to: 'tasks#destroy'  #delete
+  
+  resources :tasks
   
 end
