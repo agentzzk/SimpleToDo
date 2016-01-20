1a41f7a86802d1c20cbaacbae568f7dce33f4d35
ZainZ <zain-63@hotmail.com>
2016-01-20 19:55:12 +0000

    Fix bugs

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,6 +13,8 @@ class UsersController < ApplicationController
     @newUser = User.new(user_params)
     
     if @newUser.save
+      #create a default task also
+      @newTask = @newUser.tasks.create(:title => "Sample Task", :details => "Click edit at the bottom to change this text field.\r\n\r\nCreate an unordered list:\r\n<ul>\r\n<li>Item 1</li>\r\n<li>Item 2</li>\r\n</ul>\r\n\r\nOr a numbered list:\r\n<ol>\r\n<li>Item 1</li>\r\n<li>Item 2</li>\r\n</ol>\r\n\r\nUse HTML tags to further style the text.")
       flash[:success] = "Account created! Please login to use the app."
       redirect_to login_path
       
